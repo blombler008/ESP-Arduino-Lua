c9b51497480990242cdf9730d14098f107f52fed
blombler008 <blombler008@gmail.com>
2023-03-03 18:48:46 +0100

    Update LuaWrapper.h

--- src/LuaWrapper.h
+++ src/LuaWrapper.h
@@ -14,10 +14,10 @@ class LuaWrapper {
     String Lua_doFile(const char *file);
     void Lua_register(const String name, const lua_CFunction function);
     lua_State* L();
+    String addConstants();
 
   private:
     lua_State *_state;
-    String addConstants();
 };
 
 #endif
