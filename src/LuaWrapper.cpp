67910bc2872822f2f65eb5ceb7223e1af8055285
blombler008 <blombler008@gmail.com>
2023-03-03 19:04:10 +0100

    Update LuaWrapper.cpp

--- src/LuaWrapper.cpp
+++ src/LuaWrapper.cpp
@@ -21,6 +21,39 @@ extern "C" {
     Serial.println();
     return 0;
   }
+
+  static int lua_wrapper_pinMode(lua_State *lua_state) {
+    int a = luaL_checkinteger(lua_state, 1);
+    int b = luaL_checkinteger(lua_state, 2);
+    pinMode(a, b);
+    return 0;
+  }
+
+  static int lua_wrapper_digitalWrite(lua_State *lua_state) {
+    int a = luaL_checkinteger(lua_state, 1);
+    int b = luaL_checkinteger(lua_state, 2);
+    digitalWrite(a, b);
+    return 0;
+  }
+    
+  static int lua_wrapper_delay(lua_State *lua_state) {
+    int a = luaL_checkinteger(lua_state, 1);
+    delay(a);
+    return 0;
+  }
+
+  static int lua_wrapper_millis(lua_State *lua_state) {
+    lua_pushnumber(lua_state, (lua_Number) millis());
+    return 1;
+  } 
+  
+
+  static int lua_wrapper_mikros(lua_State *lua_state) {
+    lua_pushnumber(lua_state, (lua_Number) esp_timer_get_time());
+    return 1;
+  } 
+  
+
 } 
 
 lua_State* LuaWrapper::L() {
@@ -34,10 +67,15 @@ LuaWrapper::LuaWrapper() {
   luaopen_string(_state);
   luaopen_math(_state);
   lua_register(_state, "print", lua_wrapper_print);
+  lua_register(_state, "pinMode", lua_wrapper_pinMode);
+  lua_register(_state, "digitalWrite", lua_wrapper_digitalWrite);
+  lua_register(_state, "delay", lua_wrapper_delay);
+  lua_register(_state, "millis", lua_wrapper_millis); 
+  lua_register(_state, "mikros", lua_wrapper_mikros);
 }
 
 String LuaWrapper::Lua_dostring(const String *script) {
-  String scriptWithConstants = addConstants() + *script;
+  String scriptWithConstants = *script;
   String result;
   if (luaL_dostring(_state, scriptWithConstants.c_str())) {
     result += "# lua error:\n" + String(lua_tostring(_state, -1));
